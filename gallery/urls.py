f2b1edfa8cc97d795c453e674f47d472eda52c20
MaTachi <dannie92@gmail.com>
2013-10-20 18:02:27 +0200

    Add functiontality for uploading images

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, url
-from gallery.views import ImageList
+from gallery.views import ImageList, ImageCreate
 
 urlpatterns = patterns('',
    url(r'^$', ImageList.as_view(), name='list'),
+   url(r'^upload/$', ImageCreate.as_view(), name='upload'),
 )
--- gallery/views.py
+++ gallery/views.py
@@ -1,7 +1,25 @@
-# Create your views here.
-from django.views.generic import ListView
+from django.contrib import messages
+from django.core.urlresolvers import reverse
+from django.views.generic import ListView, CreateView
 from gallery.models import Image
 
 
 class ImageList(ListView):
     model = Image
+
+
+class ImageCreate(CreateView):
+    model = Image
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['url'] = reverse('gallery:upload')
+        return context
+
+    def form_valid(self, form):
+        messages.success(self.request, 'The image was successfully uploaded.')
+        return super().form_valid(form)
+
+    @staticmethod
+    def get_success_url():
+        return reverse('gallery:upload')
